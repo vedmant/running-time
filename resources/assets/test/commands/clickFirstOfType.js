b6c86c8e84483e962bb0f2635331163e4ccdcb70
Vedmant <vedmant@gmail.com>
2018-04-30 23:19:51 +0300

    Removed Nightwatch tests, use only Cypress

--- resources/assets/test/commands/clickFirstOfType.js
+++
@@ -1,9 +0,0 @@
-/**
- * Click the first item of this type based on CSS selector.
- *
- * @param {string} selector
- * @returns {exports}
- */
-exports.command = function (selector) {
-  return this.click(selector + ':first-of-type');
-};
