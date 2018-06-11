ba4cb60169a0757a1d91790f8cb00e62abc5bd11
Vedmant <vedmant@gmail.com>
2018-06-11 17:31:04 +0300

    Updated config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Jogging Time'),
+    'name' => env('APP_NAME', 'Running Time'),
 
     /*
     |--------------------------------------------------------------------------
