f9eac2b67cd889080e256ed2aa60b4644e2ec2d2
Vedmant <vedmant@gmail.com>
2024-02-15 11:20:49 +0700

    Fix

--- resources/assets/js/components/pages/Policy.vue
+++ resources/assets/js/components/pages/Policy.vue
@@ -4,7 +4,6 @@
       <h1>Privacy Policy</h1>
       <p>Last updated: February 15, 2024</p>
       <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
-      <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.termsfeed.com/privacy-policy-generator/" target="_blank">Privacy Policy Generator</a>.</p>
       <h2>Interpretation and Definitions</h2>
       <h3>Interpretation</h3>
       <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
